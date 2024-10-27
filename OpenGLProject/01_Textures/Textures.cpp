6c76065507f980e91726a1dc78fdd65942444967
RuichangBao <1294838346@qq.com>
2024-10-27 21:40:48 +0800

    aaa

--- OpenGLProject/01_Textures/Textures.cpp
+++ OpenGLProject/01_Textures/Textures.cpp
@@ -1,3 +1,4 @@
+//https://github.com/LearnOpenGL-CN/LearnOpenGL-CN/blob/new-theme/docs/01%20Getting%20started/06%20Textures.md
 #include"Textures.h"
 #include"Textures.h"
 #include"LearnOpenGL/Shader_s.h"
