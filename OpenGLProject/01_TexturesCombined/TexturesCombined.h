7f4d0dc5149cb7d572696ad7720cef56485942d2
RuichangBao <1294838346@qq.com>
2024-11-01 18:45:39 +0800

    Texture 测试

---
+++ OpenGLProject/01_TexturesCombined/TexturesCombined.h
@@ -0,0 +1,10 @@
+#include <GLFW/glfw3.h>
+
+using namespace std;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+// settings
+const unsigned int SCR_WIDTH = 800;//unsigned ÎÞ·ûºÅ
+const unsigned int SCR_HEIGHT = 600;
