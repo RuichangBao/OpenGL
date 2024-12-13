a72cd7b0105b17eddc584aab08f5c851cdb55fa4
RuichangBao <1294838346@qq.com>
2024-12-13 17:26:36 +0800

    glm

---
+++ OpenGLProject/01_07_Transformations/Transformations.cpp
@@ -0,0 +1,16 @@
+#include "Transformations.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!" << endl;
+	return 0;
+}
