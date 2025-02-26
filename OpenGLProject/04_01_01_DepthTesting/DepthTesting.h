22a8d321ad41dec046f6d047a7d170e16c93934f
RuichangBao <1294838346@qq.com>
2025-02-27 01:19:03 +0800

    aaa

--- OpenGLProject/04_01_01_DepthTesting/DepthTesting.h
+++ OpenGLProject/04_01_01_DepthTesting/DepthTesting.h
@@ -1,1 +1,29 @@
+#include <GLFW/glfw3.h>
+#include <learnopengl/camera.h>
 
+// 屏幕宽高
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+
+//相机
+Camera camera(glm::vec3(0.0f, 0.0f, 4.5f));
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+
+// timing
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);//窗口大小回调函数
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void processInput(GLFWwindow* window);
+
+//自定义日志输出
+void Print(glm::mat4 mat);
+void Print(glm::vec3 vec);
+void Print(glm::vec4 vec);
