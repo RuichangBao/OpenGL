743ed9eafce2486357adf12e420c9d1730c64fe4
RuichangBao <1294838346@qq.com>
2025-05-30 15:15:09 +0800

    aaa

---
+++ OpenGLProject/04_09_01_GeometryShaderHouses/GeometryShaderHouses.h
@@ -0,0 +1,14 @@
+#include <GLFW/glfw3.h>
+
+// фад╩©М╦ъ
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+float points[] = {
+    -0.5f,  0.5f, 1.0f, 0.0f, 0.0f, // top-left
+     0.5f,  0.5f, 0.0f, 1.0f, 0.0f, // top-right
+     0.5f, -0.5f, 0.0f, 0.0f, 1.0f, // bottom-right
+    -0.5f, -0.5f, 1.0f, 1.0f, 0.0f  // bottom-left
+};
