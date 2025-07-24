301a8319b8e617abbadc0ca6a8af2ce6a817e649
RuichangBao <1294838346@qq.com>
2025-07-24 11:52:28 +0800

    小行星带

---
+++ OpenGLProject/04_10_03_Asteroids/Asteroids.cpp
@@ -0,0 +1,191 @@
+#include "Asteroids.h"
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <stbImage/stb_image.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <learnopengl/filesystem.h>
+#include <learnopengl/shader.h>
+#include <learnopengl/camera.h>
+#include <learnopengl/model.h>
+
+using namespace std;
+
+int main()
+{
+	//glfw: 初始化配置
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);//主版本3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);//次版本3
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);//OpenGL的配置文件和属性 核心模式
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);//允许修改窗口大小
+#endif
+	// glfw 创建窗口对象
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "InstancingQuads", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "创建GLFW窗口失败" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);//使指定窗口的上下文为调用线程的当前上下文。
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);//设置窗口大小变化的回调函数
+	glfwSetCursorPosCallback(window, mouse_callback);	//设置鼠标输入回调函数
+	glfwSetScrollCallback(window, scroll_callback);		//设置鼠标滚轮
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	// glad: 加载所有OpenGL函数指针
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		cout << "初始化GLAD(OpenGL函数指针错误)失败" << endl;
+		return -1;
+	}
+	glEnable(GL_DEPTH_TEST);//开启透明度测试
+	// 构建并编译shader程序
+	Shader shader("shader/Vertex.shader", "shader/Fragment.shader");
+	shader.use();
+
+	//偏移信息
+	glm::vec2 translations[100];
+	int index = 0;
+	float offset = 0.1f;
+	for (int y = -10; y < 10; y += 2)
+	{
+		for (int x = -10; x < 10; x += 2)
+		{
+			glm::vec2 translation;
+			translation.x = (float)x / 10.0f + offset;
+			translation.y = (float)y / 10.0f + offset;
+			translations[index++] = translation;
+		}
+	}
+	unsigned int instanceVBO;
+	glGenBuffers(1, &instanceVBO);
+	glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec2) * 100, &translations[0], GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+
+	unsigned int VBO, VAO;//顶点缓冲对象
+	// 0. 复制顶点数组到缓冲中供OpenGL使用
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);//生成顶点缓存对象VBO(Vertex Buffer Object)对象
+	// 1. 绑定VAO
+	glBindVertexArray(VAO);
+	// 2. 把顶点数组复制到缓冲中供OpenGL使用
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), quadVertices, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(2 * sizeof(float)));
+	//设置实例数据
+	glEnableVertexAttribArray(2);
+	glBindBuffer(GL_ARRAY_BUFFER, instanceVBO); // 这个属性来自不同的顶点缓冲区
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glVertexAttribDivisor(2, 1); // 告诉了OpenGL该什么时候更新顶点属性的内容至新一组数据
+	//循环渲染
+	while (!glfwWindowShouldClose(window))
+	{
+		float currentFrame = static_cast<float>(glfwGetTime());
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+		//glfwFocusWindow(window);//强制获取焦点
+		//输入检查
+		processInput(window);
+
+		// 渲染
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);//清空屏幕所用的颜色
+		//glClear(GL_COLOR_BUFFER_BIT);//清空颜色缓冲区
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // GL_DEPTH_BUFFER_BIT:清除深度缓存
+
+
+		//模型矩阵
+		glm::mat4 model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(0.0f, 0.0f, -5.0f)); // translate it down so it's at the center of the scene
+		model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));	// it's a bit too big for our scene, so scale it down
+		shader.setMat4("model", model);
+
+		//观察矩阵
+		glm::mat4 view = camera.GetViewMatrix();
+		shader.setMat4("view", view);
+
+		//投影矩阵
+		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+		shader.setMat4("projection", projection);
+
+		/*	glBindVertexArray(quadVAO);
+			glDrawArrays(GL_TRIANGLES, 0, 6);*/
+
+		glBindVertexArray(VAO);
+		glDrawArraysInstanced(GL_TRIANGLES, 0, 6, 100);
+		//glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		glBindVertexArray(0);
+
+		// glfw: 交换缓冲区和轮询IO事件（按键按/释放，鼠标移动等）
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	//终止，清除之前分配的所有glfw资源。
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+
+	//终止，清除之前分配的所有glfw资源。
+	glfwTerminate();
+	return 0;
+}
+
+
+//键盘输入
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)//如果ESC（GLFW_KEY_ESCAPE）键被按下（GLFW_PRESS）
+		glfwSetWindowShouldClose(window, true);	//关闭窗口
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.ProcessKeyboard(FORWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.ProcessKeyboard(LEFT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.ProcessKeyboard(RIGHT, deltaTime);
+}
+/// <summary>鼠标输入</summary>
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
+{
+	float xpos = static_cast<float>(xposIn);
+	float ypos = static_cast<float>(yposIn);
+
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+	lastX = xpos;
+	lastY = ypos;
+	camera.ProcessMouseMovement(xoffset, yoffset);
+}
+//鼠标滚轮回调函数
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.ProcessMouseScroll(static_cast<float>(yoffset));
+}
+// 窗口大小变化回调函数
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	//cout << "窗口大小改变" << width << "  " << height << endl;
+	glViewport(0, 0, width, height);//确保视口匹配新的窗口尺寸；
+}
---
+++ OpenGLProject/04_10_03_Asteroids/Shader/Vertex.shader
@@ -0,0 +1,21 @@
+/*默认正方体顶点着色器**/
+#version 330 core
+layout(location = 0) in vec2 aPos;      //顶点位置
+layout(location = 1) in vec3 aColor;    //颜色
+layout(location = 2) in vec2 offset;    //偏移量
+
+uniform mat4 model;//模型矩阵	
+uniform mat4 view;//观察矩阵
+uniform mat4 projection;//投影矩阵
+// uniform vec2 offsets[100];
+out vec3 fColor;
+
+void main()
+{
+    fColor = aColor;
+    fColor = vec3(gl_InstanceID/100.0f, gl_InstanceID/100.0f, 1.0f);
+    // fColor = vec3(gl_InstanceID/100.0f, 0, 0);
+    // vec2 offset = offsets[gl_InstanceID];
+    // gl_Position = projection * view * model * vec4(aPos + offset, 0.0, 1.0);
+    gl_Position = vec4(aPos + offset, 0.0, 1.0);
+}
