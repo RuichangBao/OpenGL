f10d1e78bbd558ed816579ac23ebd89e844d6920
RuichangBao <1294838346@qq.com>
2025-08-13 15:14:48 +0800

    aaa

--- 05_03_02_ShadowMappingBase/ShadowMappingBase.cpp
+++ 05_03_02_ShadowMappingBase/ShadowMappingBase.cpp
@@ -18,7 +18,7 @@ int main()
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);//允许修改窗口大小
 #endif
 	// glfw 创建窗口对象
-	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "ShadowMappingDepth", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "ShadowMappingBase", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "创建GLFW窗口失败" << std::endl;
